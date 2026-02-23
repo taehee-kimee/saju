a1727fd835ea7638a8849d76a1e921bac3bc09d1
빌게이츠 <billi@nyangsae.dev>
2026-02-23 15:22:48 +0900

    feat: add 20 cat characters data and mapper with tests

---
+++ lib/catMapper.ts
@@ -0,0 +1,14 @@
+import { CATS } from '@/data/cats';
+import { CatCharacter, CatId, MbtiGroup, Ohaeng } from '@/types';
+
+export function getCatId(mbtiGroup: MbtiGroup, ohaeng: Ohaeng): CatId {
+  return `${mbtiGroup}_${ohaeng}` as CatId;
+}
+
+export function getCatCharacter(
+  mbtiGroup: MbtiGroup,
+  ohaeng: Ohaeng
+): CatCharacter {
+  const id = getCatId(mbtiGroup, ohaeng);
+  return CATS[id];
+}
